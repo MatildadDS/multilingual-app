ceba062b268160a03cad6be1effa31f445a74e50
Matildad <mtld.desouza@gmail.com>
2021-05-30 18:36:21 +0200

    browser language and adaptability:abc:

--- src/context/langContext.js
+++ src/context/langContext.js
@@ -3,9 +3,20 @@ import React, {createContext, useState} from 'react'
 
 export const Context = createContext();
 
+const supportedLangs = ['EN', 'FR', 'ES'];
+
+let browserLang = navigator.language.slice(0,2).toUpperCase();
+console.log(browserLang);
+
+if (supportedLangs.indexOf(browserLang) === -1) {
+    console.log("not supported");
+    browserLang= "EN";
+}
+
+
 const ContextProvider = props => {
 
-    const [lang, setLang] = useState('EN')
+    const [lang, setLang] = useState(browserLang)
     console.log(lang)
 
     const toggleLang = changeLang => {
